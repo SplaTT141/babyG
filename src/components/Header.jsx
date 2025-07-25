664fe615bc6e247a0ebfb399052017117f6eedc4
Ernest <zyperis14@gmail.com>
2025-07-25 14:05:51 +0300

    babyG start

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+        <div className="container">
+            <div className="row">
+                <h1>Hello worlasd</h1>
+            </div>
+        </div>
+    )
+}
