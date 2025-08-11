a965b04c9af47e8f7cfe1022cf7d5bfe70d18563
Ernest <zyperis14@gmail.com>
2025-08-11 16:28:45 +0300

    intro section + age section WIP

--- client/src/js/templates/PublicPageTemplate.js
+++ client/src/js/templates/PublicPageTemplate.js
@@ -46,30 +46,42 @@ export class PublicPageTemplate {
                 <div class="container">
                     <div class="row">
                         <div class="intro-content">
-                            <div class="text-content col-6">
-                                <h1 class="first-line">Žavūs drabužėliai patiems</h1>
-                                <h1 class="second-line">mažiausiems</h1>
-                                <p>Atraskite mūsų patogių, stilingų ir prieinamų drabužių kolekciją, skirtą kūdikiams ir vaikams.</p>
-                                <button type="button">Apsipirkti</button>
-                            </div class="col-6">
-                            <img src="./src/assets/img/kids3.png" alt="Kids" />
-                            <div class="numbers">
-                                <div class="section1">
-                                    <h1>100+</h1>
-                                    <p>Laimingų šeimų</p>
+                            <div class="text-content col-5">
+                                <div class="text-section">
+                                    <h1 class="first-line">Žavūs drabužiai patiems</h1>
+                                    <h1 class="second-line">mažiausiems</h1>
+                                    <p>Atraskite mūsų patogių, stilingų ir prieinamų drabužių kolekciją, skirtą kūdikiams ir vaikams.</p>
+                                    <button type="button">Apsipirkti</button>
                                 </div>
-                                <div class="section2">
-                                    <h1>70+</h1>
-                                    <p>Drabužių</p>
-                                </div>
-                                <div class="section3">
-                                    <div class="rating">
-                                        <h1>4.9</h1>
-                                        <i class="fa fa-star"></i>
+                                <div class="numbers-section">
+                                    <div class="section1">
+                                        <h1>100+</h1>
+                                        <p>Laimingų <br> šeimų</p>
                                     </div>
-                                    <p>Reitingas</p>
+                                    <div class="section2">
+                                        <h1>70+</h1>
+                                        <p>Drabužių</p>
+                                    </div>
+                                    <div class="section3">
+                                        <div class="rating">
+                                            <h1>4.9</h1>
+                                            <i class="fa fa-star"></i>
+                                        </div>
+                                        <p>Reitingas</p>
+                                    </div>
                                 </div>
+                            </div class="col-6">
+                            <img src="./src/assets/img/kids3.png" alt="Kids" class="col-6 m-1"/>
                             </div>
+                    </div>
+                </div>
+            </section>
+            <section class="age">
+                <div class="container">
+                    <div class="row">
+                        <div class="age-header">
+                            <h1>Pirkite pagal amžių</h1>
+                            <p>Išsirinkite tobulus drabužius kiekvienam jūsų vaiko augimo etapui.</p>
                         </div>
                     </div>
                 </div>
